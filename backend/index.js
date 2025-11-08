76285a599a1a8aed1f1d6a247c957ed973c75d17
Sy2force <shayacoca@gmail.com>
2025-11-08 23:28:02 +0200

    Remove problematic index.js and add Node version specification

--- backend/index.js
+++
@@ -1,7 +0,0 @@
-// Entry point for production deployment
-// This file imports and starts the compiled TypeScript server
-
-const app = require('./dist/server.js').default;
-
-// The server is already started in server.js, so we just need to export the app
-module.exports = app;
